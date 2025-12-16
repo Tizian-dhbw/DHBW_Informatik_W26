a04e153c3c659758d8a3152858f70ec3bcd9f917
schmolh <hans-peter.schmollinger@online.de>
2025-12-16 08:56:40 +0100

    Update pd_01.py

--- pd_01.py
+++ pd_01.py
@@ -1,20 +1,22 @@
 import pandas as pd
 
 df = pd.read_csv('Data_dup.csv')
+#df = pd.read_excel('Data_dup3.xlsx')
 
-#df.dropna(inplace = True)
+print(df)
+df.dropna(inplace = True)
 
 print(df)
 
 
-for index,row in df.iterrows():
-    print(row)
-    print(index,row.Duration, row.Calories)
-    print()
-    if index == 5:
-        df.loc[index, 'Calories'] = 45
+# for index,row in df.iterrows():
+#     print(row)
+#     print(index,row.Duration, row.Calories)
+#     print()
+#     if index == 5:
+#         df.loc[index, 'Calories'] = 45
 
-print(df.to_string)
+# print(df.to_string)
 df.to_excel("data1_new.xlsx")
 
 
